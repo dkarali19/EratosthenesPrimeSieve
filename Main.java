9ed3d73ffad6ac71737dee56f9b790d3a2b264c1
XAplexX <demir.karali.55@hotmail.com>
2021-10-05 15:10:30 +0200

    2

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.time.chrono.Era;
 
 public class Main {
     public static void main(String[] args) {
-        EratosthenesPrimeSieve era = new EratosthenesPrimeSieve(10);
+        EratosthenesPrimeSieve era = new EratosthenesPrimeSieve(20);
         era.printPrimes();
         System.out.println();
         era.printEvenNumbers();
